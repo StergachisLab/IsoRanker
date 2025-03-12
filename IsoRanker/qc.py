a71b0bff16a58e1efef48b218b9b4debedbd9ae8
Yong-Han Hank Cheng <yhhc@z3007.hyak.local>
2025-03-11 19:33:10 -0700

    Changed the input for SRSF6 to the Sample info input file

--- IsoRanker/qc.py
+++ IsoRanker/qc.py
@@ -203,13 +203,13 @@ def process_pileup(df, reference_fasta, chromosome, position, output_file):
     - Errors encountered during processing are logged in the output file.
     """
 
-    unique_bams = df.drop_duplicates(subset=['BAM_FILE'])[['SAMPLE', 'ID', 'CYCLO_NONCYCLO', 'BAM_FILE']]
+    unique_bams = df.drop_duplicates(subset=['bam_file'])[['sample', 'patient', 'cyclo', 'bam_file']]
     
     results = [["Source", "Chromosome", "Position", "Reference_Base", "Original_Read_Depth", "Exon_Read_Depth", "Exonic_Proportion", "Read_Bases", "Base_Qualities"]]
     
     for _, row in unique_bams.iterrows():
-        source = f"{row['SAMPLE']}_{row['ID']}_{row['CYCLO_NONCYCLO']}"
-        bam_file = row['BAM_FILE']
+        source = f"{row['sample']}_{row['ID']}_{row['cyclo']}"
+        bam_file = row['bam_file']
         print(f"Processing BAM file: {bam_file} from source: {source}")
         
         if pd.notna(bam_file) and os.path.exists(bam_file):
