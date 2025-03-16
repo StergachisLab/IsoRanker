b7f272d5f7a5205f40f1f4606ccfc83d2cb9b32c
Yong-Han Hank Cheng <yhhc@z3007.hyak.local>
2025-03-15 21:34:40 -0700

    Changed console script for PB data

--- setup.py
+++ setup.py
@@ -18,9 +18,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "isoranker_run_analysis=IsoRanker.run_analysis:main",
-            "run_analysis_top_ranked=IsoRanker.run_analysis_top_ranked:main",
-            "run_analysis_rds=IsoRanker.run_analysis_rds:main",
+            "isoranker_pb_run_analysis=IsoRanker.isoranker_pb_run_analysis:main",
         ],
     },
     author="Hank Cheng",
