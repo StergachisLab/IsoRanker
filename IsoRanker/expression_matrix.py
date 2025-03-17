fdc981ac44e45bc21e9ae4e4c33f25a79b056435
Yong-Han Hank Cheng <yhhc@n3462.hyak.local>
2025-03-16 17:47:18 -0700

    Index should be outputted by the expression matrix function

--- IsoRanker/expression_matrix.py
+++ IsoRanker/expression_matrix.py
@@ -54,7 +54,7 @@ def create_expression_matrix(file_path, output_file=None):
     
     # Save to a CSV file if specified
     if output_file:
-        df.to_csv(output_file, index=False, compression="gzip")
+        df.to_csv(output_file, index=True, compression="gzip")
     
     return df
 
