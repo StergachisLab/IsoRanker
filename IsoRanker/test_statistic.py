e1b76e491e5e7074154e7f0b35ea7a18caac7c57
Yong-Han Hank Cheng <yhhc@z3007.hyak.local>
2025-09-03 15:38:08 -0700

    Commented out minor isoform filter

--- IsoRanker/test_statistic.py
+++ IsoRanker/test_statistic.py
@@ -374,6 +374,7 @@ def process_hypothesis_test(filtered_data, group_col, test_statistic_func, gene_
         #    z_scored_data = z_scored_data[(z_scored_data["isoform_cyclo_proportion"] > 0.1) | 
         #                                  (z_scored_data["isoform_noncyclo_proportion"] > 0.1)]
 
+
         if test_statistic_func == NMD_rare_steady_state_transcript:
             z_scored_data = z_scored_data[z_scored_data["bin_proportion_difference"] > 0]
             z_scored_data = z_scored_data[z_scored_data["Total_bin_cyclo_count_Bin1_le"] > 10]
