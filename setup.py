b6434faf5cb2ab99cec01e428348375b99efcc59
Yong-Han Hank Cheng <yhhc@z3007.hyak.local>
2026-03-12 21:59:03 -0700

    Added console script that uses long format df instead of read_stat

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ setup(
             "isoranker_pb_run_analysis_full_with_isoform_allelic_omim=IsoRanker.isoranker_pb_run_analysis_full_with_isoform_allelic_omim:main",
             "isoranker_pb_run_analysis_full_with_isoform_allelic_no_novel=IsoRanker.isoranker_pb_run_analysis_full_with_isoform_allelic_no_novel:main",
             "isoranker_pb_run_analysis_full_with_isoform_allelic_protein_coding=IsoRanker.isoranker_pb_run_analysis_full_with_isoform_allelic_protein_coding:main",
-            "isoranker_pb_run_analysis_full_with_isoform_allelic_hap_NMD=IsoRanker.isoranker_pb_run_analysis_full_with_isoform_allelic_hap_NMD:main"
+            "isoranker_pb_run_analysis_full_with_isoform_allelic_hap_NMD=IsoRanker.isoranker_pb_run_analysis_full_with_isoform_allelic_hap_NMD:main",
+            "isoranker_pb_run_analysis_full_with_isoform_allelic_long_df=IsoRanker.isoranker_pb_run_analysis_full_with_isoform_allelic_long_df:main"
         ],
     },
     author="Hank Cheng",
